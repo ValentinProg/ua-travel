e5d91b01facdf37b5f4b37164bf827c3c9cb55e2
Valentin <V.Kovalenko.prog@gmail.com>
2022-09-23 16:01:59 +0300

    add HeroSection.css

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import { Routes, Route, Link } from 'react-router-dom'
 import Navbar from './components/Navbar';
+import Home from './components/pages/Home';
 
 function App() {
   return (
     <>
       {/* <Navbar/> */}
       <Routes>
-        <Route path='/' element={<Navbar/>}/>
+        <Route path='/' element={<Home/>}/>
         <Route path='/products' element={<Navbar/>}/>
         <Route path='/services' element={<Navbar/>}/>
         <Route path='/sign-up' element={<Navbar/>}/>
